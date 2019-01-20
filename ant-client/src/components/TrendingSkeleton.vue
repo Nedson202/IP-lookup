b68e21ec1626478c1e8e7b25b694b881e9b7f2c9
Nedson202 <simeonsamson4@gmail..com>
2019-01-20 02:16:56 +0100

    Add twitter trend component and preloader

---
+++ ant-client/src/components/TrendingSkeleton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <a-col :span="6">
+      <a-skeleton active />
+    </a-col>
+    <a-col :span="6">
+      <a-skeleton active />
+    </a-col>
+    <a-col :span="6">
+      <a-skeleton active />
+    </a-col>
+    <a-col :span="6">
+      <a-skeleton active />
+    </a-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrendingSkeleton'
+}
+</script>
---
+++ ant-client/src/components/TweetCard.vue
@@ -0,0 +1,15 @@
+<template>
+<div style="background:#ECECEC; padding:30px">
+  <a-card title="Card title" :bordered="false" style="width: 100%">
+    <p>Card content</p>
+    <p>Card content</p>
+    <p>Card content</p>
+  </a-card>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TweetCard'
+}
+</script>
